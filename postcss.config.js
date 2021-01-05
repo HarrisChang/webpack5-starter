a09a8aca05ece47a34ac5f38c90d36e5a4938f6f
张奇 <jone09@outlook.com>
2021-01-05 16:15:43 +0800

    init es6 project by webpack

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: zhangqi
+ * @Date: 2021-01-05 15:36:33
+ * @Description: 
+ * @LastEditTime: 2021-01-05 15:37:03
+ * @LastEditors: zhangqi
+ * @FilePath: /webpack5-starter-ts/postcss.config.js
+ */
+module.exports = {
+    plugins: {
+        "postcss-preset-env": {
+            browsers: 'last 2 versions'
+        }
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+/*
+ * @Author: zhangqi
+ * @Date: 2021-01-05 15:11:31
+ * @Description: webpack main config file
+ * @LastEditTime: 2021-01-05 16:08:05
+ * @LastEditors: zhangqi
+ * @FilePath: /webpack5-starter-ts/webpack.config.js
+ */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist')
+    },
+    module: {
+        rules: [
+            // js
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            // images
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource'
+            },
+            // fonts and svgs
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+                type: 'asset/inline'
+            },
+            // css, postcss, sass
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'hello webpack',
+            template: path.resolve(__dirname, './src/index.template.html'),
+            filename: 'index.html'
+        }),
+        /**每次构建后清除 dist 目录下所有内容 */
+        new CleanWebpackPlugin(),
+        // HMR
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        port: 8080,
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        compress: true,
+        hot: true
+    }
+}
